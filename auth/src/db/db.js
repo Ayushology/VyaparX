90770343d7259d67cf555b7afa441f4a930dbc66
Ayush Kumar <ash09aps@gmail.com>
2026-07-10 21:10:56 +0530

    fix: correct function name and improve error handling in database connection

--- auth/src/db/db.js
+++ auth/src/db/db.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-async function connectTodb() {
-    try{
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("Db is connected successfully");
-        
-    }catch(err){
-        console.log(err);
-    }
+async function connectToDb() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("Database connected successfully.");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+
+    process.exit(1);
+  }
 }
 
-module.exports = connectTodb
+module.exports = connectToDb;
