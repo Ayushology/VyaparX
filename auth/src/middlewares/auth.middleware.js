36157b20a8006f36fec3355dce6f2ad89eb57779
Ayush Kumar <ash09aps@gmail.com>
2026-07-11 00:54:11 +0530

    feat: enhance auth middleware to check for blacklisted tokens

--- auth/src/middlewares/auth.middleware.js
+++ auth/src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
-
+const redis = require("../db/redis");
 async function authMiddleware(req, res, next) {
   const token = req.cookies.token;
 
@@ -9,7 +9,15 @@ async function authMiddleware(req, res, next) {
       message: "Unauthorized",
     });
   }
+  
+    const isBlacklisted = await redis.get(`blacklist:${token}`);
 
+  if (isBlacklisted) {
+    return res.status(401).json({
+      message: "Unauthorized - Token is blacklisted",
+    });
+  }
+ 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
