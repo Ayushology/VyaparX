1a00d7a50e19ea8fa6be1f03a5bf7833e64d7b88
Ayush Kumar <ash09aps@gmail.com>
2026-08-02 16:59:03 +0530

    feat: remove unused multer service import from product routes

--- product/src/routes/product.routes.js
+++ product/src/routes/product.routes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const upload = require("../services/multer");
 const productController = require("../controllers/product.controller");
 const { createAuthMiddleware } = require("../middlewares/auth.middleware");
 const { uploadProductImages } = require("../middlewares/upload.middleware");
