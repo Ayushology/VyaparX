0164972003976ad351aeae95c054223568e5fb89
Ayush Kumar <ash09aps@gmail.com>
2026-08-03 17:49:00 +0530

    feat: add validation for item addition to cart

--- cart/src/routes/cart.routes.js
+++ cart/src/routes/cart.routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const authMiddleware = require('../middlewares/auth.middleware')
 const cartController = require("../controllers/cart.controller")
-router.post("/items",authMiddleware.createAuthMiddleware["buyer"],cartController.addItemToCart)
+const validation = require('../validators/items.validator')
+router.post("/items",validation.validateItemToCart,authMiddleware.createAuthMiddleware["buyer"],cartController.addItemToCart)
 
 module.exports = router
--- cart/src/validators/items.validator.js
+++ cart/src/validators/items.validator.js
@@ -1,1 +1,32 @@
 const {body,validationResult} = require('express-validator')
+
+function validateResult(req, res, next){
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        success: false,
+        message: "Validation failed",
+        errors: errors.array().map((error) => ({
+          field: error.path,
+          message: error.msg,
+        })),
+      });
+    }
+    next();
+  }
+
+const validateItemToCart = [
+   body("productId")
+    .trim()
+    .notEmpty()
+    .withMessage("Product ID is required.")
+    .isString()
+    .withMessage("Product ID must be a valid string."),
+    body("quantity")
+    .optional() 
+    .toInt()  
+    .isInt({ min: 1 })
+    .withMessage("Quantity must be an integer of at least 1."),
+]
+
+module.exports = {validateItemToCart}
