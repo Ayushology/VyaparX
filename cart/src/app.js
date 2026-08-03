403d7222ee08cdc665724379070cffd4b6b5b34a
Ayush Kumar <ash09aps@gmail.com>
2026-08-03 23:10:02 +0530

    feat: add spacing for improved readability in app.js

--- cart/src/app.js
+++ cart/src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+
+// Importing Routes
 const cartRoutes = require("../src/routes/cart.routes")
 const cookieParser = require('cookie-parser')
+
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
 app.use("/api/cart",cartRoutes)
 module.exports = app;
