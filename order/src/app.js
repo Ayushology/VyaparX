294c39fea0d757d5ec53de930ece595eb4cccabb
Ayush Kumar <ash09aps@gmail.com>
2026-08-04 17:49:40 +0530

    feat: update order API endpoint and add order model with schema definitions

--- order/src/app.js
+++ order/src/app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const orderRoutes = require('./routes/order.routes')
 const app = express();
 
-app.use('/api/order',orderRoutes);
+app.use('/api/orders',orderRoutes);
 
 module.exports = app;
