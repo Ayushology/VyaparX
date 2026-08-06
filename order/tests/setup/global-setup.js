bc578efddb0fec20fc73fa24e0c300ef7dac200d
Ayush Kumar <ash09aps@gmail.com>
2026-08-06 19:12:28 +0530

    refactor: remove obsolete order integration tests and MongoDB setup files

--- order/tests/setup/global-setup.js
+++
@@ -1,9 +0,0 @@
-const { MongoMemoryServer } = require("mongodb-memory-server");
-
-module.exports = async () => {
-  const mongoServer = await MongoMemoryServer.create();
-
-  global.__MONGOSERVER__ = mongoServer;
-
-  process.env.MONGO_URI = mongoServer.getUri();
-};
--- order/tests/setup/global-teardown.js
+++
@@ -1,5 +0,0 @@
-module.exports = async () => {
-  if (global.__MONGOSERVER__) {
-    await global.__MONGOSERVER__.stop();
-  }
-};
