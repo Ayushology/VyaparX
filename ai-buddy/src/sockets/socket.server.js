a1e3fb3fc31e8cb2c35a7d48611fc5c19b701675
Ayush Kumar <ash09aps@gmail.com>
2026-08-09 13:27:58 +0530

    feat: add product search and cart management tools; enhance socket server with JWT authentication

--- ai-buddy/src/sockets/socket.server.js
+++ ai-buddy/src/sockets/socket.server.js
@@ -1,13 +1,32 @@
-const { createServer } = require("http");
+const { Server } = require("socket.io");
+const jwt = require("jsonwebtoken");
+const cookie = require("cookie");
 
 async function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
+
+  io.use((socket, next) => {
+    const cookies = socket.handshake.headers?.cookie;
+    const { token } = cookies ? cookie.parseCookie(cookies) : {};
+
+    if (!token) {
+      return next(new Error("Token not found"));
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      socket.user = decoded;
+
+      next();
+    } catch (err) {
+      next(new Error("Invalid Credentials"));
+    }
+  });
 
   io.on("connection", (socket) => {
     console.log("A user connected");
   });
 }
 
-httpServer.listen(3000);
-
-module.exports = {};
+module.exports = { initSocketServer };
