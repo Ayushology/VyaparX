0c203be23bfd1031ed29d83c44f51a5684701002
Ayush Kumar <ash09aps@gmail.com>
2026-08-11 23:55:03 +0530

    feat: add RabbitMQ connection and messaging functionality; implement publish and subscribe methods

--- auth/server.js
+++ auth/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const app = require('./src/app')
+const {connect} = require('./src/broker/broker')
 const connectTodb = require('./src/db/db')
 
 connectTodb();
+connect();
 app.listen(3000,()=>{
     console.log("Server is listening at port 3000");
 })
