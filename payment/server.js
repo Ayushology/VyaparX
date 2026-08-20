30992b69d3829d9ac997928b8395fe7083999aa1
Ayush Kumar <ash09aps@gmail.com>
2026-08-20 13:18:19 +0530

    feat(database): implement MongoDB connection logic and update server initialization

--- payment/server.js
+++ payment/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
+const connectToDb = require('./src/config/db')
 const app = require('./src/app')
-
+connectToDb()
 app.listen(3004,()=>{
-    console.log("server is running on port 3004");
+    console.log("Payment service is running on port 3004");
 })
