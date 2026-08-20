d85d154e65d3e78fdb464cd9093a34b06cb0473e
Ayush Kumar <ash09aps@gmail.com>
2026-08-20 13:18:12 +0530

    feat(payment): add payment model, authentication middleware, and routes

---
+++ payment/src/routes/payment.routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const createAuthMiddleware = require('../middlewares/auth.middleware')
+
+const router = express.Router();
+
+router.post('/',createAuthMiddleware(['user']),)
