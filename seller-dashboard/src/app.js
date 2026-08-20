7f9bb4d6e71d0bffb3c0ac6e4c023869529dea0d
Ayush Kumar <ash09aps@gmail.com>
2026-08-21 04:05:21 +0530

    feat(seller-dashboard): set up Express app and add seller-dashboard routes

--- seller-dashboard/src/app.js
+++ seller-dashboard/src/app.js
@@ -1,4 +1,11 @@
 const express  = require('express')
+
+const cookieParser = require('cookie-parser');
 const app = express();
+const sellerDashboardRoute = require('./routes/sellerdashboard.routes')
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/seller-dashboard',sellerDashboardRoute);
 
 module.exports = app;
