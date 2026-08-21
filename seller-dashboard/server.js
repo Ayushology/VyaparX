0fbea75dade4d9ac3098de61911657abb450edd4
Ayush Kumar <ash09aps@gmail.com>
2026-08-21 18:01:41 +0530

    feat(broker): add RabbitMQ connection and messaging functions; set up database connection

--- seller-dashboard/server.js
+++ seller-dashboard/server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const app = require('./src/app')
-
-
+const connectToDb = require('./src/config/db');
+connectToDb()
 app.listen(3007,()=>{
     console.log("Seller-Dashboard is running at port 3007");
 });
